a4c7f1e4258009582a8a75de1d438d718a636b94
aastha624 <patelaastha2910@gmail.com>
2024-04-24 14:16:02 +0530

    two way binding and handling events

--- 01-starting-project (2)/01-starting-project/src/App.jsx
+++ 01-starting-project (2)/01-starting-project/src/App.jsx
@@ -1,9 +1,14 @@
 
 import Header from "./component/Header.jsx"
+import Userinput from "./component/Userinput.jsx"
 function App() {
   return (
+    <>
 
     <Header/> 
+    <Userinput/>
+    
+    </>
     // custom header component
   )
 }
--- 01-starting-project (2)/01-starting-project/src/component/Userinput.jsx
+++ 01-starting-project (2)/01-starting-project/src/component/Userinput.jsx
@@ -1,25 +1,58 @@
 //  here first export component function-- and id add using css folder and take id name
- export default function Userinput(){
-return <section id ="user-input">
-<div classname="input-group">
-<p>
-    <label>Initial Investment</label>
-    <input type="number" required/>
-</p>
-<p>
-    <label>Annual Investment</label>
-    <input type="number" required/>
-</p>
-</div>
-<div classname="input-group">
-<p>
-    <label>Expected Return</label>
-    <input type="number" required/>
-</p>
-<p>
-    <label>Duration</label>
-    <input type="number" required/>
-</p>
-</div>
-</section>
- }
+import { useState } from "react";
+
+
+export default function Userinput() {
+    const [userInput, setuserInput] = useState({
+        initialInvestment: 10000,
+        annualInvesetment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+
+    });
+    // create generatic function
+
+    function handlechange(inputindentifier, newvalue) {
+        setuserInput(prevuserInput => {
+            return {
+                ...prevuserInput,
+                [inputindentifier]: newvalue
+            };
+        })
+    }
+    return <section id="user-input">
+        <div classname="input-group">
+            <p>
+                <label>Initial Investment</label>
+                <input type="number" required
+                    value={userInput.initialInvestment}
+                    onChange={(event) => handlechange(' initialInvestment', event.target.value)
+                    } />
+            </p>
+            <p>
+                <label>Annual Investment</label>
+                <input type="number" required
+                    value={userInput.annualInvesetment}
+                    onChange={(event) => handlechange('annualInvesetment', event.target.value)
+                    } />
+              
+            </p>
+        </div>
+        <div classname="input-group">
+            <p>
+                <label>Expected Return</label>
+                <input type="number" required
+                    value={userInput.expectedReturn}
+                    onChange={(event) => handlechange('expectedReturn', event.target.value)
+                    } />
+            </p>
+            <p>
+                <label>Duration</label>
+                <input type="number" required
+                    value={userInput.duration}
+                    onChange={(event) => handlechange(' duration', event.target.value)
+                    } />
+            </p>
+        </div>
+    </section>
+}
